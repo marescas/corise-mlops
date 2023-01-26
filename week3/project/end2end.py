8f9ce317ab3981a60e69c265880a37c3ab98befd
Marcos Esteve Casademunt <marescasinf@gmail.com>
2023-01-26 20:27:27 +0100

    Update end2end.py

--- week3/project/end2end.py
+++ week3/project/end2end.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     with open('project/data/requests.json', 'r') as f:
         queries = [json.loads(line) for line in f]
 
-    endpoint = "http://0.0.0.0:8000/predict"
+    endpoint = "http://0.0.0.0:80/predict"
     for q in queries:
         print(requests.post(endpoint, data=json.dumps(q)).json())
